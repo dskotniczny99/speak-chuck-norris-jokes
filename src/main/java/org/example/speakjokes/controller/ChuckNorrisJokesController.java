0119fdc5f315a1fede425da7a79eeda68549e687
Daniel <danielskotniczny1999@gmail.com>
2024-03-15 12:41:42 +0100

    added /jokes controller, thymeleaf

--- src/main/java/org/example/speakjokes/controller/ChuckNorrisJokesController.java
+++ src/main/java/org/example/speakjokes/controller/ChuckNorrisJokesController.java
@@ -3,7 +3,12 @@ package org.example.speakjokes.controller;
 import lombok.AllArgsConstructor;
 import org.example.speakjokes.api.ChuckNorrisJokesApiResponse;
 import org.example.speakjokes.service.ChuckNorrisJokesService;
+import org.example.speakjokes.service.VoiceRssService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -13,9 +18,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ChuckNorrisJokesController {
 
     private final ChuckNorrisJokesService chuckNorrisJokesService;
+    private final VoiceRssService voiceRssService;
 
-
-    public ChuckNorrisJokesApiResponse randomJoke() {
-        return chuckNorrisJokesService.randomJoke();
+    @GetMapping
+    public String jokeView() {
+        return "jokes";
     }
+
+    @PostMapping
+    public String joke(ModelMap modelMap) {
+        ChuckNorrisJokesApiResponse apiResponse = chuckNorrisJokesService.randomJoke();
+        modelMap.addAttribute("joke", apiResponse.getValue());
+        new Thread(() -> voiceRssService.speakJoke(apiResponse.getValue())).start();
+        return "jokes";
+    }
+
 }
